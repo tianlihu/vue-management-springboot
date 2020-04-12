d2c06c5c2118bd19d12823e03e9b625402155afc
tianlihu <tianlihu@qq.com>
2020-04-12 17:17:42 +0800

    服务器端代码框架

---
+++ server/commons/src/main/java/com/tianlihu/management/commons/RequestTool.java
@@ -0,0 +1,7 @@
+package com.tianlihu.management.commons;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RequestTool {
+}
---
+++ server/management/src/test/java/com/tianlihu/management/ManagementTests.java
@@ -0,0 +1,12 @@
+package com.tianlihu.management;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManagementTests {
+
+    @Test
+    public void tests() {
+    }
+}
