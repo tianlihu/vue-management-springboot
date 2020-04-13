2f5c1da25d8d712f89f771581dbfb8459c0435fd
tianlihu <tianlihu@qq.com>
2020-04-13 19:27:19 +0800

    把MyBatis Plus的Mapper扫描移动到分页设置类上

--- server/management/src/main/java/com/tianlihu/management/ManagementApplication.java
+++ server/management/src/main/java/com/tianlihu/management/ManagementApplication.java
@@ -1,10 +1,8 @@
 package com.tianlihu.management;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.tianlihu.management")
 @SpringBootApplication
 public class ManagementApplication {
 
