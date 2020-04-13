49374b89a19be37b5cbb174ea7494b867153878d
tianlihu <tianlihu@qq.com>
2020-04-13 17:07:28 +0800

    配置vue axios跨域功能

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    baseUrl: './',
+    publicPath: './',
     assetsDir: 'static',
     productionSourceMap: false,
-    // devServer: {
-    //     proxy: {
-    //         '/api':{
-    //             target:'http://jsonplaceholder.typicode.com',
-    //             changeOrigin:true,
-    //             pathRewrite:{
-    //                 '/api':''
-    //             }
-    //         }
-    //     }
-    // }
-}
+    devServer: {
+        proxy: {
+            '/rest': {
+                target: 'http://localhost:9090/', //对应自己的接口
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/rest': ''
+                }
+            }
+        }
+    }
+};
