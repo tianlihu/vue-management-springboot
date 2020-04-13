e97975d75fa612ba070e212293a22d248567e8ce
tianlihu <tianlihu@qq.com>
2020-04-13 16:30:08 +0800

    后端项目基本功能正常运行

--- server/management/src/main/java/com/tianlihu/management/ManagementApplication.java
+++ server/management/src/main/java/com/tianlihu/management/ManagementApplication.java
@@ -1,8 +1,10 @@
 package com.tianlihu.management;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.tianlihu.management")
 @SpringBootApplication
 public class ManagementApplication {
 
