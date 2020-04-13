be2a8d82819cf79799d8157d10186d19ae2d4874
tianlihu <tianlihu@qq.com>
2020-04-13 19:26:41 +0800

    用户查询条件处理

--- server/management/src/main/java/com/tianlihu/management/query/UserQuery.java
+++ server/management/src/main/java/com/tianlihu/management/query/UserQuery.java
@@ -5,13 +5,15 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Date;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @ApiModel("用户查询条件")
 public class UserQuery extends PageQuery {
 
     @ApiModelProperty(value = "部门ID", notes = "部门ID")
-    private String departmentId;
+    private Integer departmentId;
     @ApiModelProperty(value = "登录名", notes = "登录名")
     private String account;
     @ApiModelProperty(value = "姓名", notes = "姓名")
@@ -19,11 +21,7 @@ public class UserQuery extends PageQuery {
     @ApiModelProperty(value = "电话", notes = "电话")
     private String phone;
     @ApiModelProperty(value = "管理员(0非管理员, 1管理员)", notes = "管理员(0非管理员, 1管理员)")
-    private String admin;
-    @ApiModelProperty(value = "最后登录时间", notes = "最后登录时间")
-    private String lastLoginTime;
-    @ApiModelProperty(value = "登录次数", notes = "登录次数")
-    private String loginTimes;
+    private Boolean admin;
     @ApiModelProperty(value = "状态(0未启用, 1启用)", notes = "状态(0未启用, 1启用)")
-    private String status;
+    private Boolean status;
 }
